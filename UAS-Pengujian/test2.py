5a2649d72a14f63f93e217c3b5634102405fa790
rizkisimanullang <114975993+rizkisimanullang@users.noreply.github.com>
2024-06-18 05:50:15 +0800

    Delete UAS-Pengujian/test2.py

--- UAS-Pengujian/test2.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-driver.get("https://www.geeksforgeeks.org/")
-
-while(True):
-    pass
